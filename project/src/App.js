4b9313b4bed96744c2f7c3c45e36bc01a1dad9de
Matheus Cardoso <46722962+MatheusCTorres@users.noreply.github.com>
2022-01-01 20:19:31 +0000

    first commit

---
+++ project/src/App.js
@@ -0,0 +1,70 @@
+import "./App.css";
+import { useEffect, useState } from "react";
+import image1 from "./image/img1.jpeg";
+import image2 from "./image/img2.jpeg";
+import image3 from "./image/img3.jpeg";
+
+function App() {
+  const [index, setIndex] = useState(0);
+
+  const mod = (n, m) => {
+    let result = n % m;
+
+    // Return a positive value
+    return result >= 0 ? result : result + m;
+  };
+
+  const cards = [
+    {
+      id: "1",
+      image: image1,
+    },
+    {
+      id: "2",
+      image: image2,
+    },
+    {
+      id: "3",
+      image: image3,
+    },
+  ];
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIndex((index + 1) % cards.length);
+      console.log(index);
+    }, 3000);
+  }, [index]);
+
+  return (
+    <div className="App">
+      <div className="carousel">
+        {cards.map((item, i) => {
+          const indexLeft = mod(index - 1, cards.length);
+          const indexRight = mod(index + 1, cards.length);
+
+          let className = "card";
+
+          if (i === index) {
+            className = "card card--active";
+          } else if (i === indexRight) {
+            className = "card card--right";
+          } else if (i === indexLeft) {
+            className = "card card--left";
+          } else className = "card";
+
+          return (
+            <img
+              key={item.id}
+              className={className}
+              src={item.image}
+              alt="Comic"
+            ></img>
+          );
+        })}
+      </div>
+    </div>
+  );
+}
+
+export default App;
